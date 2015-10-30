d6766933e7b72e898b202eca395f426bcc4cd71e
Mariano López Minnucci <mlopezviedma@gmail.com>
2015-10-30 01:57:45 -0300

    Script es más elegante

--- get_files.sh
+++ get_files.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
+# Script para descargar las fuentes y la documentación de LFS 7.7-systemd
 
-wget="wget -c"
+wget="wget --continue"
 
 lfs_url=http://www.linuxfromscratch.org/lfs/downloads/7.7-systemd
 packages_url=ftp://ftp.lfs-matrix.net/pub/lfs/lfs-packages/7.7-systemd
 
 lfs_html=LFS-BOOK-7.7-systemd-NOCHUNKS.html
 lfs_pdf=LFS-BOOK-7.7-systemd.pdf
-#sources_file=wget-list
 md5sums_file=md5sums
 
 archiso_dir=archiso-troya
@@ -22,13 +22,12 @@ $wget $lfs_url/$lfs_html
 $wget $lfs_url/$lfs_pdf
 
 cd $old_wd
-
 cd $archiso_dir/$sources_dir
 
 $wget $lfs_url/$md5sums_file
 cat $md5sums_file | while read line; do
 	echo "$line" | md5sum -c &>/dev/null || \
-	$wget $packages_url/$(echo $line | cut -d " " -f 2)
+		$wget $packages_url/$(echo $line | cut -d " " -f 2)
 done
 rm $md5sums_file
 
